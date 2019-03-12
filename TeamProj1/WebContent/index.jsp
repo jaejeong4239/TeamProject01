badc590a42d3a170d57b8f0302dbf3df04167b70
KOSMO <KOSMO@DESKTOP-95RS7O0>
2019-03-12 12:06:39 +0900

    학원에서 두번째 파일생성 , 수정

--- TeamProj1/WebContent/index.jsp
+++ TeamProj1/WebContent/index.jsp
@@ -10,7 +10,14 @@
 
 	<ul>
 		<li>학원에서 만든 index 파일</li>
-		<li>첫번째 수정</li>
+		<li>파일 수정:KOSMO</li>
+		
+		<li>
+			<%
+			DTO dto =	new DTO("홍길동");
+			out.println(dto.getName());
+			%>
+		</li>
 	</ul>
 
 </body>
---
+++ TeamProj1/src/DTO.java
@@ -0,0 +1,22 @@
+
+public class DTO {
+	
+	private String name;
+
+
+	public DTO(String name) {
+		this.name = name;
+	}
+	
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	
+	
+
+}
